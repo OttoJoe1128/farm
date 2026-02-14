7493d6b975293d597b51ebd18b8955e046d194e0
sircaedebiyat <sircaedebiyat@gmail.com>
2026-02-14 22:01:33 +0000

    a

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,46 +1,17 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
-  channel = "unstable";
-
+  channel = "stable-24.05";
   packages = [
+    pkgs.python311
+    pkgs.python311Packages.fastapi
+    pkgs.python311Packages.uvicorn
+    pkgs.python311Packages.python-multipart
+    pkgs.python311Packages.pip  # Bunu da ekleyelim ne olur ne olmaz
     pkgs.flutter
-    pkgs.python3
-    pkgs.python3Packages.pip
-    pkgs.php
-    pkgs.composer
-    pkgs.nodejs_20
-    pkgs.cmake
   ];
-
-  env = {};
-
-  idx = {
-    extensions = [
-      "dart-code.flutter"
-      "dart-code.dart-code"
-    ];
-
+  idx.previews = {
+    enable = true;
     previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = [ "sh" "-c" '''
-            cd smartfarm_xr && flutter run -d web-server --web-port=$PORT
-          ''' ];
-          manager = "web";
-        };
-      };
-    };
-
-    workspace = {
-      onCreate = {
-        install-py-root-deps = "pip install -r requirements.txt";
-        install-py-backend-deps = "pip install -r backend/requirements.txt";
-        install-flutter-deps = "cd smartfarm_xr && flutter pub get";
-        install-php-deps = "composer install";
-      };
-      onStart = {};
+      # Burası kalsın
     };
   };
 }
